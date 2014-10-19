de0c978ee2a73e53ff10417522a970d8831ec708
Varietyy <chasedss@gmail.com>
2014-10-19 01:26:06 -0400

    Update Shmurdabot.js

--- Shmurdabot.js
+++ Shmurdabot.js
@@ -3119,7 +3119,7 @@ diceCommand: {
 
             rouletteCommand: {
                 command: 'roulette',
-                rank: 'manager',
+                rank: '5',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
