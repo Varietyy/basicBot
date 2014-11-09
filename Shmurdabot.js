cb7a88d3e13141739880e40d21bfceaa24762704
Varietyy <chasedss@gmail.com>
2014-11-09 13:12:44 -0500

    Update Shmurdabot.js

--- Shmurdabot.js
+++ Shmurdabot.js
@@ -787,15 +787,15 @@
         },
         eventDjadvance: function (obj) {
            var lastplay = obj.lastPlay;
--            if (basicBot.settings.songstats) {
--            if (typeof basicBot.chat.songstatistics === "undefined") {
--                   API.sendChat(subChat(basicBot.chat.songstatistics, {title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
-- }
--                 else {
--                   API.sendChat(subChat(basicBot.chat.songstatistics, {title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
--}
--             }
-+
+            if (basicBot.settings.songstats) {
+            if (typeof basicBot.chat.songstatistics === "undefined") {
+                   API.sendChat(subChat(basicBot.chat.songstatistics, {title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
+ }
+                 else {
+                   API.sendChat(subChat(basicBot.chat.songstatistics, {title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
+}
+             }
+
             basicBot.room.roomstats.totalWoots += lastplay.score.positive;
             basicBot.room.roomstats.totalMehs += lastplay.score.negative;
             basicBot.room.roomstats.totalCurates += lastplay.score.grabs;
