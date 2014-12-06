aa9fa70b9c11a954b3bf22ef673bbfc671c89e54
Varietyy <chasedss@gmail.com>
2014-12-05 22:05:56 -0500

    Update thotbot.js

--- thotbot.js
+++ thotbot.js
@@ -2894,16 +2894,6 @@ API.sendChat(":sparkles: "+ data.un +" gives props to @"+ API.getDJ().username +
 }
 });
 
-/*API.on(API.CHAT, function(data){
-    
-if(data.message.indexOf('!slap') === 0){
-API.moderateDeleteChat(data.cid);
-var a = API.getUsers();
-for(var i in a){
-if(a[i].username === data.message.substr(10)){
-API.sendChat('@' + data.message.substr(10) + ', ' + data.from + ' bitch slapped you!');
-}*/
-
 (function(){
         var skipping = false, skipThreshold = 8;
         API.on(API.SCORE_UPDATE,function(score){
